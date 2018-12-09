efe3f0acff5a0e66f51c81ae3e0e2ad04d4ee2f8
tayyy <10136313+tayyyTF@users.noreply.github.com>
2018-12-09 00:26:23 -0800

    Added up to lgsm install.

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+DAEMON_ACCOUNT="TF2SERVERDAEMON"
+SPACER="========================================"
+echo "~~Root user check in progress~~"
+if [ "$EUID" -ne 0 ]
+  then
+      echo "[ERROR] This installation script must be run with root privileges!"
+      echo "Exiting....."
+  exit
+fi
+echo "Root privileges verified successfully!"
+echo $SPACER
+echo "Please enter your logs.tf API key:"
+read LOGS_TF_API_KEY
+echo "Please enter your demos.tf API key:"
+read DEMOS_TF_API_KEY
+echo $SPACER
+echo "Installing dependencies..."
+dpkg --add-architecture i386
+apt update
+apt install mailutils postfix curl wget file bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux lib32gcc1 libstdc++6 libstdc++6:i386 libcurl4-gnutls-dev:i386 libtcmalloc-minimal4:i386
+echo "Finished installation of dependencies"
+echo $SPACER
+echo "Preparing for server installation"
+echo "Generating daemon user account..."
+adduser --system --shell '/bin/bash' --disabled-login $DAEMON_ACCOUNT
+sudo -i -u $DAEMON_ACCOUNT bash << EOF
+echo "Taking control of $DAEMON_ACCOUNT"
+wget -O linuxgsm.sh https://linuxgsm.sh 
+chmod +x linuxgsm.sh 
+./linuxgsm.sh tf2server
+./tf2server install
+EOF
+
+
+
+
+
+
+
